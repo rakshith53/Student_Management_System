febcb9d6351e8c43393711c4cb5b8b2f4d6ddb3b
Rakshith <rakshithravi5@gmail.com>
2023-01-08 16:25:38 +0530

    Creating Student Class

---
+++ src/student_database_app/Student.java
@@ -0,0 +1,60 @@
+package student_database_app;
+
+import java.util.Scanner;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private String studentID;
+    private String courses;
+    private int tuitionBalance;
+    private static int costOfCourse = 600;
+    private static int id = 1001;
+
+    //Constructor : student's name and year
+    public Student() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Student first name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter Student last name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("1 - Freshmen\n2 - Sophomore\n3 - Senior\n4 - Senior\nEnter student class level");
+        this.gradeYear = in.nextInt();
+        setStudentID();
+        System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+
+
+    }
+    //Generate Id
+    private void setStudentID() {
+        //Grade level + ID
+        this.studentID = gradeYear + "" + id;
+        id++;
+    }
+    //Enroll Course
+    public void enroll(){
+        do {
+            System.out.print("Enter course to enroll (Q to quit)");
+            Scanner sc = new Scanner(System.in);
+            String course = sc.nextLine();
+            if (!course.equals("Q")) {
+                courses += ("\n" + course);
+                tuitionBalance += costOfCourse;
+            }
+            else break;
+        }while (true);
+    }
+    //View balance
+    public void viewBalance() {
+        System.out.println("Your balance is: $" + tuitionBalance);
+    }
+    //Pay tuition
+    public void payTuition(int payment){
+        tuitionBalance = payment;
+    }
+    //Show status
+
+}
---
+++ src/student_database_app/StudentDatabaseApp.java
@@ -0,0 +1,8 @@
+package student_database_app;
+
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+        //Ask number of students
+    }
+}
